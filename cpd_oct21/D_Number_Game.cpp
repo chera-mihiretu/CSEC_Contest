1e797a12c74d63a8b6c86f63df075564fb05f950
chera-mihiretu <cheramihiretucm@gmail.com>
2025-10-22 13:52:11 +0300

    d answer

--- cpd_oct21/D_Number_Game.cpp
+++ cpd_oct21/D_Number_Game.cpp
@@ -8,28 +8,46 @@
 
 using namespace std;    
 typedef long long ll;
-int solve () {
-    int n;
-    cin >> n;
-    Arr(int) arra(n);
-    Arr(int) arrb(n);
 
-    for (int i = 0; i < n; i ++) {
-        cin >> arra[i] >> arrb[i];
-    }
+bool good(vector<int> nums, int k) {
+    sort(all(nums));
+    for (int i = 1; i <= k; i++) {
+        int target = k - i + 1;
+        int index = upper_bound(all(nums), target) - nums.begin() - 1;
 
-
-    Arr(Pair(int)) dp(n + 1);
-
-    for (int i = 0; i < n; i ++) {
+        if (index < 0) return false;
+        nums.erase(nums.begin() + index);
+        if (nums.empty()) return true;
+        nums[0] += target;
+        sort(all(nums));
         
     }
+    return true;
+}
 
+int solve() {
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
 
-
-
+    int left = 1, right = n, ans = 0;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (good(nums, mid)) {
+            ans = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    cout << ans << endl;
+    return ans;
 }
 
 int main() {
-    cout << solve() << endl;
+
+    int t;
+    cin >> t;
+    while (t--) solve();            
 }
